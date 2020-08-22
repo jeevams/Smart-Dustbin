5e852557ebd0f63da253cd2419b178065fda7457
jeevams <58686474+jeevams@users.noreply.github.com>
2020-08-23 01:39:16 +0530

    Update smartdustbin.c

--- smartdustbin.c
+++ smartdustbin.c
@@ -1,51 +1,41 @@
-#include <Servo.h>   //servo library
-Servo servo;     
-int trigPin = 5;    
-int echoPin = 6;   
-int servoPin = 7;
-int led= 10;
-long duration, dist, average;   
-long aver[3];   //array for average
+#include <Servo.h>//servo library
+Servo servoMain; //object for server motor
+int trigpin = 9; //sending signal pin
+int echopin = 8;//rec signal pin
+int distance;
+float duration;
 
 
-void setup() {       
-    Serial.begin(9600);
-    servo.attach(servoPin);  
-    pinMode(trigPin, OUTPUT);  
-    pinMode(echoPin, INPUT);  
-    servo.write(0);         //close cap on power on
-    delay(100);
-    servo.detach(); 
-} 
-
-void measure() {  
- digitalWrite(10,HIGH);
-digitalWrite(trigPin, LOW);
-delayMicroseconds(5);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(15);
-digitalWrite(trigPin, LOW);
-pinMode(echoPin, INPUT);
-duration = pulseIn(echoPin, HIGH);
-dist = (duration/2) / 29.1;    //obtain distance
+void setup()
+{
+servoMain.attach(7); //set a pin
+pinMode(trigpin, OUTPUT);
+pinMode(echopin, INPUT);
 }
-void loop() { 
-  for (int i=0;i<=2;i++) {   //average distance
-    measure();               
-   aver[i]=dist;            
-    delay(10);              //delay between measurements
-  }
- dist=(aver[0]+aver[1]+aver[2])/3;    
 
-if ( dist<50 ) {
-//Change distance as per your need
- servo.attach(servoPin);
-  delay(1);
- servo.write(0);  
- delay(3000);       
- servo.write(150);    
- delay(1000);
- servo.detach();      
-}
-Serial.print(dist);
+void loop()
+{
+digitalWrite(trigpin, LOW);// clear a pin
+    delay(2);
+
+ digitalWrite(trigpin, HIGH);//send a signal fro 10 microseconds and the clear;
+delayMicroseconds(10);
+digitalWrite(trigpin, LOW);
+    
+duration = pulseIn(echopin, HIGH);//get a out from a echo pin;
+                                    
+distance= duration*0.034/2;
+                          //vel=dist/time;
+                          //dis=time*vel;
+                         //vel=0.034cm/micro sec;
+
+ if(distance<30)
+  { 
+    servoMain.write(180); // delay(3000); 
+  } 
+ else
+   { 
+     servoMain.write(0); 
+     delay(50); 
+   } 
 }
